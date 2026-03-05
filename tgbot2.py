a021448463f2e7fa2a4cc244cc24a9f36855317b
mdsaafirahman-netizen <mdsaafirahman@gmail.com>
2026-03-05 18:41:42 +0600

    Refactor password error handling in tgbot2.py
    Removed error handling for wrong password response.

--- tgbot2.py
+++ tgbot2.py
@@ -74,15 +74,6 @@ async def check_password(update: Update, context: ContextTypes.DEFAULT_TYPE):
             reply_markup=InlineKeyboardMarkup(keyboard)
         )
 
-    else:
-         msg = await
-    update.message.reply_text("❌ Wrong password. Try again.")
-    await asyncio.sleep(5)
-    try:
-        await msg.delete()
-    except:
-        pass
-
 # SHOW LIST
 async def show_list(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
